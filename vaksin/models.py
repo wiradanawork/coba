7b257851eb59b3753ccbe6cdcebcc4145c74845a
kaachiii <kaachi579@gmail.com>
2025-04-27 02:33:08 +0700

    add model vaksin

--- vaksin/models.py
+++ vaksin/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Vaksin(models.Model):
+    kode = models.CharField(max_length=6, primary_key=True)
+    nama = models.CharField(max_length=50, null=False)
+    harga = models.IntegerField(null=False)
+    stok = models.IntegerField(null=False)
+
+    def __str__(self):
+        return f"{self.nama} (Kode: {self.kode})"
+
+    class Meta:
+        managed = False
+        db_table = "vaksin"
