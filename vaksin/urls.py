509a874fdd3288191a23934177c6c612bcb70074
kaachiii <kaachi579@gmail.com>
2025-04-28 02:12:07 +0700

    fe vaccination with dummy

--- vaksin/urls.py
+++ vaksin/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('update-vaksin/<str:kode>/', update_vaksin, name='update_vaksin'),
     path('update-stok-vaksin/<str:kode>/', update_stok_vaksin, name='update_stok_vaksin'),
     path('delete-vaksin/<str:kode>/', delete_vaksin, name='delete_vaksin'),
+    path('list-vaksinasi/', list_vaksinasi, name='list_vaksinasi'),
+    path('create-vaksinasi/', create_vaksinasi, name='create_vaksinasi'),
+    path('update-vaksinasi/<str:id>/', update_vaksinasi, name='update_vaksinasi'),
+    path('delete-vaksinasi/<str:id>/', delete_vaksinasi, name='delete_vaksinasi'),
 ]
--- vaksin/views.py
+++ vaksin/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from .models import *
 from .forms import *
+from django.template import loader
+from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponseNotFound
 
 def list_vaksin(request):
     vaksin_list = Vaksin.objects.all()
@@ -50,3 +54,38 @@ def delete_vaksin(request, kode):
     vaksin.delete()
     return redirect('vaksin:list_vaksin')
 
+vaccinations = [
+    {"id": "1", "kunjungan": "Kunjungan 1", "tanggal": "2025-01-01", "vaksin": "Vaksin A"},
+    {"id": "2", "kunjungan": "Kunjungan 2", "tanggal": "2025-01-05", "vaksin": "Vaksin B"},
+    {"id": "3", "kunjungan": "Kunjungan 3", "tanggal": "2025-01-10", "vaksin": "Vaksin C"},
+]
+
+def list_vaksinasi(request):
+    return render(request, 'list_vaccination.html', {'vaccinations': vaccinations})
+
+def delete_vaksinasi(request, id):
+    global vaccinations
+    vaccinations = [vacc for vacc in vaccinations if vacc['id'] != id]
+    messages.success(request, "Vaksinasi berhasil dihapus.")
+    return redirect('vaksin:list_vaksinasi')
+
+def create_vaksinasi(request): 
+    template = loader.get_template('create_vaccination.html')
+    return HttpResponse(template.render())
+
+def update_vaksinasi(request, id):
+    vaccination = next((v for v in vaccinations if v['id'] == str(id)), None)
+    if vaccination is None:
+        return HttpResponseNotFound('Vaccination not found')
+
+
+    if request.method == 'POST':
+        vaksin = request.POST.get('vaksin')
+        vaccination['vaksin'] = vaksin
+        messages.success(request, f"Vaksinasi untuk {vaccination['kunjungan']} telah diperbarui.")
+        return redirect('vaksin:list_vaksinasi')
+
+    context = {
+        'vaccination': vaccination
+    }
+    return render(request, 'update_vaccination.html', context)
