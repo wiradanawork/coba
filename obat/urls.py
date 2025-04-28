bf7c329d61af3803ee4f520da3980e0c5b0b7645
emanuellaabygail <abygailsiahaan1911@gmail.com>
2025-04-28 16:04:12 +0700

    redesign forms

--- obat/urls.py
+++ obat/urls.py
@@ -6,7 +6,7 @@ app_name = 'obat'
 urlpatterns = [
     path('', list_obat, name='list_obat'),
     path('create-obat/', create_obat, name='create_obat'),
-    path('update-obat/<str:kode_obat>/', update_obat, name='update_obat'),
-    path('update-stok/<str:kode_obat>/', update_stok_obat, name='update_stok_obat'),
-    path('delete-obat/<str:kode_obat>/', delete_obat, name='delete_obat'),
+    path('update-obat/<str:kode>/', update_obat, name='update_obat'),
+    path('update-stok/<str:kode>/', update_stok_obat, name='update_stok_obat'),
+    path('delete-obat/<str:kode>/', delete_obat, name='delete_obat'),
 ]
