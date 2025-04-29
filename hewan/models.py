8b06a653910072089a534a9bff417a903e58de5c
Rachel Mathilda <rachel.mathilda@ui.ac.id>
2025-04-29 18:41:21 +0700

    edit html file di hewan dan main

--- hewan/models.py
+++ hewan/models.py
@@ -5,12 +5,18 @@ def generate_uuid():
     return uuid.uuid4()
 
 class Hewan(models.Model):
-    id = models.UUIDField(primary_key=True, default=generate_uuid, editable=False)  # Added for Django compatibility
+    id = models.UUIDField(primary_key=True, default=generate_uuid, editable=False)
     nama = models.CharField(max_length=50)
     no_identitas_klien = models.ForeignKey(Klien, on_delete=models.CASCADE, db_column='no_identitas_klien')
     tanggal_lahir = models.DateField()
     id_jenis = models.ForeignKey(models.Jenis_hewan, on_delete=models.CASCADE, db_column='id_jenis')
     url_foto = models.CharField(max_length=25)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.nama} (Kode: {self.kode})"
 
     class Meta:
         db_table = 'hewan'
@@ -19,6 +25,17 @@ class Hewan(models.Model):
 class Jenis_hewan(models.Model):
     id = models.UUIDField(primary_key=True, default=generate_uuid, editable=False)
     nama_jenis = models.CharField(max_length=50)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            last_trm = Jenis_hewan.objects.all().order_by('id').last()
+            if last_trm:
+                last_code = last_trm.kode_perawatan
+                next_code = f"HWN{int(last_code[3:]) + 1:03d}"
+            else:
+                next_code = "HWN001"
+            self.kode_perawatan = next_code
+        super().save(*args, **kwargs)
 
     class Meta:
         db_table = 'jenis_hewan'
