526211f4b5f2078f74a5187a275a2cd9f1f15e48
rayyan <wiradana.work@gmail.com>
2025-05-28 19:56:02 +0700

    fitur hijau

--- perawatan_hewan_1/urls.py
+++ perawatan_hewan_1/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = 'perawatan_hewan_1'
 
 urlpatterns = [
-    path('perawatan-hewan_1', views.show_perawatan_hewan, name='perawatan_hewan_1'),
-
+    path('', views.show_perawatan_hewan, name='perawatan_hewan_list'),
+    path('create/', views.create_perawatan, name='create_perawatan'),
+    path('update/', views.update_perawatan, name='update_perawatan'),
+    path('delete/', views.delete_perawatan, name='delete_perawatan'),
+    path('detail/<uuid:id_kunjungan>/<str:kode_perawatan>/', views.get_perawatan_detail, name='get_perawatan_detail'),
 ]
